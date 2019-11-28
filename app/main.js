f0ad5c2eb184b870c48c0d24acea2f0362490ab0
WaritsaraBambuttook <four_nukiz@hotmail.com>
2019-11-28 15:26:46 +0700

    notification success

--- app/main.js
+++ app/main.js
@@ -1,9 +1,11 @@
 import Vue from "nativescript-vue";
-//import noti from "./components/notification";
+import bemo from "./components/bemo";
 import login from "./components/login";
 import VueDevtools from "nativescript-vue-devtools";
 var firebase = require("nativescript-plugin-firebase");
 import RadGauge from "nativescript-ui-gauge/vue";
+var dialogs = require("tns-core-modules/ui/dialogs");
+import { store } from "./store/store";
 // import NSVueGlobalDrawer from "nativescript-vue-global-drawer";
 // Vue.use(NSVueGlobalDrawer);
 // Test commit
@@ -15,27 +17,82 @@ Vue.registerElement(
 Vue.use(RadGauge);
 firebase
   .init({
+    showNotifications: true,
+    showNotificationsWhenInForeground: true,
     onMessageReceivedCallback: function(message) {
       console.log("Title: " + message.title);
       console.log("Body: " + message.body);
+      console.log(JSON.stringify(message));
+
+      let dataInStore = store.getters.dataAboutUser;
+      console.log(dataInStore);
+      if (dataInStore != "") {
+        if (message.title) {
+          const confirmOptions = {
+            title: message.title,
+            message: message.body,
+            okButtonText: "Scan",
+            cancelButtonText: "Cancel"
+          };
+          dialogs.confirm(confirmOptions).then(result => {
+            // console.log(result);
+            if (result == true) {
+              console.log("scan button");
+              Vue.prototype.$navigateTo(bemo, {
+                props: {
+                  user: dataInStore,
+                  indexChangeTap: 1,
+                  text: "1"
+                }
+              });
+
+              // this.$navigateTo(pageApp);
+              //this.changePage = pageApp;
+            } else {
+              console.log("cancel button");
+            }
+          });
+        } else {
+          console.log("foreground == close app");
+
+          Vue.prototype.$navigateTo(bemo, {
+            props: {
+              user: dataInStore,
+              indexChangeTap: 1,
+              text: "2"
+            }
+          });
+        }
+      } else {
+        console.log("go to login");
+        dialogs.alert("Pleasa first login and help people").then(function() {
+          console.log("Dialog closed!");
+        });
+        Vue.prototype.$navigateTo(login);
+      }
+
+      // if(){}
+
       // if your server passed a custom property called 'foo', then do this:
       // console.log("Value of 'foo': " + message.data.foo);
-      const confirmOptions = {
-        title: message.title,
-        message: message.body,
-        okButtonText: "Scan",
-        cancelButtonText: "Cancel"
-      };
-      confirm(confirmOptions).then(result => {
-        console.log(result);
-        if (result == true) {
-          console.log("scan button");
-          // this.$navigateTo(pageApp);
-          //this.changePage = pageApp;
-        } else {
-          console.log("cancel button");
-        }
-      });
+      // const confirmOptions = {
+      //   title: message.title,
+      //   message: message.body,
+      //   okButtonText: "Scan",
+      //   cancelButtonText: "Cancel"
+      // };
+      // dialogs.confirm(confirmOptions).then(result => {
+      //   console.log(result);
+      //   if (result == true) {
+      //     console.log("scan button");
+      //     Vue.prototype.$navigateTo(bemo);
+
+      //     // this.$navigateTo(pageApp);
+      //     //this.changePage = pageApp;
+      //   } else {
+      //     console.log("cancel button");
+      //   }
+      // });
     },
 
     onPushTokenReceivedCallback: function(token) {
@@ -45,6 +102,8 @@ firebase
   .then(
     function() {
       console.log("firebase.init done 555");
+      // let dataInStore = store.getters.dataAboutUser;
+      // console.log(dataInStore);
       firebase
         .subscribeToTopic("news")
         .then(() => console.log("Subscribed to topic"));
